94694bef5f998131ba1dd8c3343f3446f604de55
Danilo Santos <danilosantosweb@hotmail.com>
2025-10-01 00:48:34 -0300

    Reajuste interface geral

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import customtkinter as ctk
 
 # Configurações globais de aparência
-ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("dark-blue")
+ctk.set_appearance_mode("light") # aparência da janela (e sistema) opções: "System", "Dark", "Light"
+ctk.set_default_color_theme("blue") # personalizado (poucos ajustes) temas: blue, dark-blue, green, etc.
 
 class App(ctk.CTk):
     def __init__(self):
-        super().__init__()   
+        super().__init__()
 
         # Configurações da janela
         self.title("App do Danilo")            # rótulo - barra de título
-        self.geometry("700x400")               # tamanho que a janela abre
+        self.geometry("700x400")               # tamanho que a janela abre (atual)
         self.configure(fg_color="#FFFFFF")   # cor de fundo 
-        self.minsize(400, 300)                 # tamanho mínimo
-        self.maxsize(1000, 700)                # tamanho máximo
-        self.resizable(True, True)             # janela ajustável
+        # self.minsize(400, 300)               # tamanho mínimo (p/ minimizar)
+        # self.maxsize(1000, 700)              # tamanho máximo (p/ maximizar)
+        self.resizable(True, True)             # janela ajustável (agora ativado)
         self.iconbitmap()                      # inserir arquivo de ícone (opcional)
 
         # Transparência (alguns sistemas ignoram - usando em try/except)
@@ -29,11 +29,11 @@ class App(ctk.CTk):
     def create_widgets(self):
         title = ctk.CTkLabel(
                              self, 
-                             text="Testando recursos CustomTkinter", 
-                             font=ctk.CTkFont(size=22, weight="bold")
+                             text="Testando recursos CustomTkinter", font=ctk.CTkFont(size=22, 
+                             weight="bold")
         )
         # Coloca o widget na janela (pack ou grid)
-        title.pack(pady=20)
+        title.pack(pady=20) # ajusta altura do título
 
 if __name__ == "__main__":
     app = App()
